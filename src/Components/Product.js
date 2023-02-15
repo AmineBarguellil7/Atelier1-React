74bdcb89d31184f2ee1f43b62c96f7f23d83fa5b
lemhafamine <aminebarguellil7@gmail.com>
2023-02-15 12:57:06 +0100

    Atelier1-update2

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,37 +1,49 @@
 import { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import './Product.css';
+import React from 'react';
 
 
 
 function Product(props) {
   
-  
   const  [like,setLike]=useState(props.like);
   const clickHandler = () => {
-    setLike(like + 1);
-  };
+     setLike(like + 1);
+   };
+
+   const [quantite,setQuantite]=useState(props.quantity);
+   
+
+   const decrementQuantity=()=> {
+    props.buy();
+    if (quantite>0) {
+      setQuantite(quantite-1);
+    }
+   }
   
   
   
   return (
+    <div className='div1'>
         <Card className="card">
-          <Card.Img  variant="top"  src={require('../assets/images/'+props.img)} width="286" height="180" />
+          <Card.Img    src={require('../assets/images/'+props.img)} width="286" height="180" />
           <Card.Body>
-            <Card.Title>{props.title}</Card.Title>
+            <Card.Title>{props.name}</Card.Title>
             <Card.Text>
             Price : {props.price} DT
             </Card.Text>
             <Card.Text>
-              Quantity : {props.quantite}
+              Quantity : {quantite}
             </Card.Text>
             <Card.Text>
               Likes : {like}
             </Card.Text>
             <button className='btn btn-primary' onClick={clickHandler}>Like</button>
-            <button className='btn btn-info' disabled={props.quantite===0} onClick={props.buy} >Buy</button>
+            <button className='btn btn-info' disabled={quantite===0} onClick={decrementQuantity} >Buy</button> 
           </Card.Body>
         </Card>
+        </div>
       );
 }
 
--- src/Components/Products.js
+++ src/Components/Products.js
@@ -4,26 +4,24 @@ import Product from "./Product";
 
 function Products() {
 
-
+  const products = require("../Data/products.json");
 
   const [showAlert, setShowAlert] = useState(false);
 
-  const buy = () => {
-    setShowAlert(true);
-    setTimeout(() => setShowAlert(false), 2000);
-  };
-
-  const products = require("../Data/products.json");
-
   const [showMsg, setShowMsg] = useState(false);
 
-  useEffect(() => {
-    setShowMsg(true);
+  useEffect(() => {       //useEffect prend deux parametres fonction wa array thot fih les variables ki titbadel valeur mta3 ayy variable fonction elli fi wost useEffect titlanca mara okhra.
+    setShowMsg(true);     //useEffect(()=> {},[]) kan yitnaha tableau yo93od m3a kol changement fel composant titlanca le fonction.
     setTimeout(() => {
       setShowMsg(false);
     }, 3000);
   }, []);
 
+
+  const buy = () => {
+    setShowAlert(true);
+  setTimeout(() => setShowAlert(false), 2000);
+};
 
 
   return (
@@ -31,34 +29,21 @@ function Products() {
       <Alert show={showMsg} variant="success">
         <Alert.Heading>Hey, Welcome To Our Shop MyStore <br /> Thank you for choosing our store, we hope you enjoy your shopping experience! </Alert.Heading>
         <hr />
-      </Alert>
-      <Product
-        title={products[0].name}
-        quantite={products[0].quantity}
-        price={products[0].price}
-        img={products[0].img}
-        like={products[0].like}
-        buy={buy}
-      />
-      <Product
-        title={products[1].name}
-        quantite={products[1].quantity}
-        price={products[1].price}
-        img={products[1].img}
-        like={products[1].like}
-        buy={buy}
-      />
-      <Product
-        title={products[2].name}
-        quantite={products[2].quantity}
-        price={products[2].price}
-        img={products[2].img}
-        like={products[2].like}
-        buy={buy}
-      />
+      </Alert> 
+      { products.map((p) => (
+        <Product
+          key={p.name}
+          name={p.name}
+          price={p.price}
+          quantity={p.quantity}
+          like={p.like}
+          img={p.img}
+          buy={buy}
+        />
+      ))  }
       <Alert show={showAlert} variant="success">
         <Alert.Heading>You bought an Item!</Alert.Heading>
-      </Alert>
+      </Alert> 
     </div>
   );
 }
